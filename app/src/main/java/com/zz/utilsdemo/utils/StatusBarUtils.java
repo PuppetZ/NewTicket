ff9345255f8c609862ae8f3777577aa2a589d61e
puppet_zz <zz_puppet@163.com>
2017-08-08 17:06:16 +0800

    更新

---
+++ app/src/main/java/com/zz/utilsdemo/utils/StatusBarUtils.java
@@ -0,0 +1,73 @@
+package com.zz.utilsdemo.utils;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by zhangjing on 2016/8/8.
+ * 状态栏颜色  透明
+ */
+
+public class StatusBarUtils {
+
+    /**
+     * 给状态栏设置颜色
+     *
+     * @param activity activity
+     */
+    public static void setStatusBarColor(Activity activity) {
+        //4.4以上
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0版本以上
+                Window window = activity.getWindow();
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);//设置flag才能调用setStatusBarColor 来设置状态栏颜色
+                window.setStatusBarColor(Color.parseColor("#2d8aff"));
+            } else {
+                Window window = activity.getWindow();
+                // Translucent status bar
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+/*
+                View mStatusBarTintView = new View(activity);
+                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 50);
+                params.gravity = Gravity.TOP;
+                mStatusBarTintView.setLayoutParams(params);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                FrameLayout contentParent = (FrameLayout) window.getDecorView().findViewById(android.R.id.content);
+                mStatusBarTintView.setBackgroundColor(Color.TRANSPARENT);
+                contentParent.addView(mStatusBarTintView);*/
+            }
+
+
+        }
+
+
+    }
+
+    /**
+     * 给状态栏设置透明的
+     *
+     * @param activity activity
+     */
+    public static void setStatusBarTransparent(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4透明
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0 全透明实现
+                Window window = activity.getWindow();
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//设置全屏模式
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);//calculateStatusColor(Color.WHITE, (int) alphaValue)
+            } else {
+                Window window = activity.getWindow();
+                // Translucent status bar
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            }
+        }
+    }
+}
--- app/src/main/java/com/zz/utilsdemo/utils/Utils.java
+++ app/src/main/java/com/zz/utilsdemo/utils/Utils.java
@@ -2,6 +2,9 @@ package com.zz.utilsdemo.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
+
+import java.security.MessageDigest;
 
 /**
  * Created by zhangjing on 2015/8/4.
@@ -24,5 +27,34 @@ public class Utils {
             sp.edit().putBoolean("run", false).commit();
         }
         return run;
+    }
+
+
+    /**
+     * MD5 加密
+     * @param string   str
+     * @return         加密后的str
+     */
+    public static String md5(String string) {
+        if (TextUtils.isEmpty(string)) {
+            return "";
+        }
+        MessageDigest messageDigest = null;
+        StringBuffer md5StrBuff = new StringBuffer();
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(string.getBytes("UTF-8"));
+
+            byte[] byteArray = messageDigest.digest();
+            for (int i = 0; i < byteArray.length; i++) {
+                if (Integer.toHexString(0xFF & byteArray[i]).length() == 1)
+                    md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+                else md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return md5StrBuff.toString().toUpperCase();//字母大写
     }
 }
