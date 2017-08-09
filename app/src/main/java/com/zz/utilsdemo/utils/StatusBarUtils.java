b04f1d1a180eaa1374ba21cff06dffa86ebab006
puppet_zz <zz_puppet@163.com>
2017-08-09 17:08:00 +0800

    更新

--- app/src/main/java/com/zz/utilsdemo/utils/StatusBarUtils.java
+++ app/src/main/java/com/zz/utilsdemo/utils/StatusBarUtils.java
@@ -3,7 +3,6 @@ package com.zz.utilsdemo.utils;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
-import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -49,6 +48,21 @@ public class StatusBarUtils {
 
 
     }
+
+   /* 可以在最外层的视图(getDecorView())上，添加一个视图view，领状态栏为透明的，在设置状态栏颜色时，就会把view的颜色展示
+   这个是解决Drawlayout 抽屉拉出来时设置状态栏颜色的思路之一
+   View mStatusBarTintView = new View(activity);
+    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 50);
+    params.gravity =Gravity.TOP;
+                mStatusBarTintView.setLayoutParams(params);
+                window.getDecorView().
+
+    setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+    FrameLayout contentParent = (FrameLayout) window.getDecorView().findViewById(android.R.id.content);
+                mStatusBarTintView.setBackgroundColor(Color.TRANSPARENT);
+                contentParent.addView(mStatusBarTintView);*/
+
 
     /**
      * 给状态栏设置透明的
